f26e04e7b42cd8df2ab387babe4138bb56e60f8c
kocesat <kocesat@gmail.com>
2022-10-23 16:24:55 +0300

    MidNumberParser fix

--- src/com/kocesat/median/MidNumberParser.java
+++ src/com/kocesat/median/MidNumberParser.java
@@ -1,47 +1,46 @@
 package com.kocesat.median;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MidNumberParser {
+    public static final Map<Integer, String> digitMap = new HashMap<>(6, 1);
+
     public static void main(String[] args) {
+        fillDigitMap();
         int[] numbers = getNumbersFromUser();
         Arrays.sort(numbers);
         String mid = String.valueOf(numbers[1]);
+        System.out.println("Median number: " + mid);
         char[] chars = mid.toCharArray();
         for (int i=0; i<chars.length; i++) {
-            System.out.printf("%s: %s%n", getWord(i), chars[i]);
+            System.out.printf("%s=%s\n", digitMap.get(i), chars[i]);
         }
     }
 
-    public static String getWord(int i) {
-        switch (i) {
-            case 5:
-                return "Birler Basamağı";
-            case 4:
-                return "Onlar Basamağı";
-            case 3:
-                return "Yüzler Basamağı";
-            case 2:
-                return "Binler Basamağı";
-            case 1:
-                return "Onbinler Basamağı";
-            case 0:
-                return "Yüzbinler Basamağı";
-            default:
-                return null;
-        }
+    private static void fillDigitMap() {
+        digitMap.put(5, "Birler");
+        digitMap.put(4, "Onlar");
+        digitMap.put(3, "Yüzler");
+        digitMap.put(2, "Binler");
+        digitMap.put(1, "Onbinler");
+        digitMap.put(0, "Yüzbinler");
     }
 
     public static int[] getNumbersFromUser() {
+        final int maxNum = 1_000_000;
         Scanner sc = new Scanner(System.in);
-        System.out.println("First Number: ");
+        System.out.println("First Number 6 digit long: \n");
         int first = sc.nextInt();
-        System.out.println("Second Number: ");
+        System.out.println("Second Number 6 digit long: \n");
         int second = sc.nextInt();
-        System.out.println("Third Number: ");
+        System.out.println("Third Number 6 digit long: \n");
         int third = sc.nextInt();
-        sc.close();
+        if (first >= maxNum || second >= maxNum || third >= maxNum) {
+            throw new IllegalArgumentException("Invalid input");
+        }
         return new int[] {first, second, third};
     }
 }
